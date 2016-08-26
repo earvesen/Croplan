55cd246ade0aa57440cd10d4368da52e7a00799e
Jen Rasmussen <jrasmussen@four51.com>
2016-08-25 21:20:05 -0500

    Checkout Item Summary / Checkout "Submit Order' button

--- app/js/app.js
+++ app/js/app.js
@@ -2,4 +2,4 @@
 
 var four51 = {};
 four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics',
-    'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-AnonRouter','OrderCloud-SpecForms', 'OrderCloud-SecurityModal', 'OrderCloud-TermsModal']);
+    'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-AnonRouter','OrderCloud-SpecForms', 'OrderCloud-CheckoutItemSummary', 'OrderCloud-SecurityModal', 'OrderCloud-TermsModal']);
---
+++ app/lib/oc/checkoutItemSummary.js
@@ -0,0 +1,3 @@
+/**
+ * Created by jen on 8/25/2016.
+ */
